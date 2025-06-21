4a3ccfa885b2fd26d462e2695a0b1eeaf458349c
znozor <iamshakil658@gmail.com>
2025-06-21 18:40:16 +0530

    up

---
+++ PharmaProject/PharmaProject/Controllers/SaleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PharmaProject.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Stock()
+        {
+            return View();
+        }
+        public IActionResult PrintBill()
+        {
+            return View();
+        }
+    }
+}
